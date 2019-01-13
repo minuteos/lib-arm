483b3d5cf479a71f3e2a9e2a59cdf1a47f31cf42
Stefan Simek <simek@triaxis.sk>
2019-01-13 22:11:13 +0100

    cortex-m: PLATFORM_DBG_ macros can be defined in a more specific platform

--- targets/cortex-m/base/platform.h
+++ targets/cortex-m/base/platform.h
@@ -28,8 +28,10 @@ typedef union { uint8_t u8; uint16_t u16; uint32_t u32; } Cortex_ITM_Port_t;
 
 EXTERN_C volatile Cortex_ITM_Port_t* Cortex_GetDebugChannel(unsigned channel);
 
+#if !defined(PLATFORM_DBG_ACTIVE) && !defined(PLATFORM_DBG_CHAR)
 #define PLATFORM_DBG_ACTIVE(channel)        ((CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk) && (ITM->TER & (1 << (channel))))
 #define PLATFORM_DBG_CHAR(channel, char)    ({ Cortex_GetDebugChannel(channel)->u8 = (char); })
+#endif
 
 typedef void (*cortex_handler_t)(void);
 
