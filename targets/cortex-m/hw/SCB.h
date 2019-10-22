f6bec884656a639b13421519f17f32876252ec99
Štefan Šimek <simek@triaxis.sk>
2019-10-22 15:37:13 +0200

    cortex-m: SCB - remove automatic IRQ clearing, leave it to the user

--- targets/cortex-m/hw/SCB.h
+++ targets/cortex-m/hw/SCB.h
@@ -39,11 +39,14 @@ public:
     void DeepSleep() { EnableDeepSleep(); WaitForInterrupt(); }
     void DisableDeepSleep() { SCR &= ~SCB_SCR_SLEEPDEEP_Msk; }
     void EnableDeepSleep() { SCR |= SCB_SCR_SLEEPDEEP_Msk; }
+    void EnableWake(IRQn_Type irq) { NVIC->ISER[irq >> 5] = NVIC->ICPR[irq >> 5] = BIT(irq & 31); }
+    void TriggerWake(IRQn_Type irq) { NVIC->ISPR[irq >> 5] = BIT(irq & 31); }
+    void DisableWake(IRQn_Type irq) { NVIC->ICER[irq >> 5] = BIT(irq & 31); }
 
     bool IRQPending() { return ICSR & SCB_ICSR_ISRPENDING_Msk; }
     IRQn_Type PendingIRQn() { return IRQn_Type(((ICSR & SCB_ICSR_VECTPENDING_Msk) >> SCB_ICSR_VECTPENDING_Pos) - NVIC_USER_IRQ_OFFSET); }
     IRQn_Type ActiveIRQn() { return IRQn_Type(((ICSR & SCB_ICSR_VECTACTIVE_Msk) >> SCB_ICSR_VECTACTIVE_Pos) - NVIC_USER_IRQ_OFFSET); }
 
-private:
-    void WaitForInterrupt();
+    //! interrupts must be disabled, but we must lift BASEPRI limit to allow any interrupt to wake us from WFI
+    ALWAYS_INLINE void WaitForInterrupt() { auto bp = __get_BASEPRI(); __set_BASEPRI(0); __WFI(); __set_BASEPRI(bp); }
 };
