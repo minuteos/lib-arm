c7f0b88c1db732593333ef4bbe574f09652ff08a
Štefan Šimek <simek@triaxis.sk>
2024-11-16 20:06:56 +0100

    test(qemu-arm): add double-word implementation to flash mock

--- targets/qemu-arm/nvram/Flash.cpp
+++ targets/qemu-arm/nvram/Flash.cpp
@@ -37,16 +37,40 @@ bool Flash::Write(const void* ptr, Span data)
     return data == Span(ptr, data.Length());
 }
 
+#if NVRAM_FLASH_DOUBLE_WRITE
+
+void Flash::ShredDouble(const void* ptr)
+{
+    ASSERT(!(uintptr_t(ptr) & 7));
+    auto p = (uint32_t*)ptr;
+    p[0] = p[1] = 0;
+}
+
+bool Flash::WriteDouble(const void* ptr, uint32_t lo, uint32_t hi)
+{
+    ASSERT(!(uintptr_t(ptr) & 7));
+    auto p = (uint32_t*)ptr;
+    p[0] &= lo;
+    p[1] &= hi;
+    return p[0] == lo && p[1] == hi;
+}
+
+#else
+
 void Flash::ShredWord(const void* ptr)
 {
+    ASSERT(!(uintptr_t(ptr) & 3));
     *(uint32_t*)ptr = 0;
 }
 
 bool Flash::WriteWord(const void* ptr, uint32_t word)
 {
+    ASSERT(!(uintptr_t(ptr) & 3));
     *(uint32_t*)ptr &= word;
     return *(uint32_t*)ptr == word;
 }
+
+#endif
 
 bool Flash::Erase(Span range)
 {
